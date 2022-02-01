dbc0f3549bcebad79613542f5db651a76515e088
Ashwaryashah <ashwaryashah2000@gmail.com>
2022-02-01 20:32:05 +0545

    commit

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "1931b58821310a043e4ee748a01cea89";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+}
